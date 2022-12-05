93fb0db24070ac93529a807d24249279eedf0aab
Andrew Bruner <andrewbruner@gmail.com>
2022-12-05 00:27:32 +0000

    Create initial todo app

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+"use strict";
+const todos = {
+    list: [],
+    view() {
+        return this.list;
+    },
+    add(text) {
+        const todo = {
+            text,
+            isComplete: false,
+        };
+        this.list = [...this.list, todo];
+        return todo;
+    },
+};
--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log('hello world');
+interface Todo {
+	text: string,
+	isComplete: boolean,
+}
+interface Todos {
+	list: Todo[],
+	view(): Todo[],
+	add(text: string): Todo,
+}
+
+const todos: Todos = {
+	list: [],
+	view() {
+		return this.list;
+	},
+	add(text) {
+		const todo = {
+			text,
+			isComplete: false,
+		}
+		this.list = [...this.list, todo];
+		return todo;
+	},
+}
